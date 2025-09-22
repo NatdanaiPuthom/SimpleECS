ced5ac375b9b1a30da176c5c575b76059e432d38
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2025-09-22 21:12:28 +0200

    Test Create component

--- Source/Launcher/MemoryPool/MemoryPool.hpp
+++ Source/Launcher/MemoryPool/MemoryPool.hpp
@@ -8,12 +8,14 @@ namespace Simple
 	public:
 		MemoryPool(const size_t aTypeSize, const size_t aTypeAlignment, const size_t aTypeHashCode, const size_t aReserveAmount = 8);
 		~MemoryPool();
-
+	public:
+		template<typename T>
+		T* CreateComponent(const T& aValue = T());
 	public:
 		size_t GetAvailableMemorySpace() const;
 		size_t GetOccupiedMemorySpace() const;
 		size_t GetCapacity() const;
-
+	public:
 		void PrintMemoryStatus() const;
 	private:
 		void Allocate(const size_t aSize);
@@ -26,4 +28,18 @@ namespace Simple
 		size_t myTypeHashCode;
 		size_t myTypeAlignment;
 	};
+
+	template<typename T>
+	inline T* MemoryPool::CreateComponent(const T& aValue)
+	{
+		while (sizeof(T) > GetAvailableMemorySpace())
+		{
+			Reallocate();
+		}
+
+		T* component = new(myCurrentMemoryAddress)T(aValue);
+		myCurrentMemoryAddress += sizeof(T);
+
+		return component;
+	}
 }
